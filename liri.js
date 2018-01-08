76cef2a083202215582f582ef0372e3174dafea5
Matt Barnett <Madison@Madisons-MacBook-Pro.local>
2018-01-07 20:50:44 -0500

    commented out dotenv now not getting tweets in only getting console log that says this is loaded

--- liri.js
+++ liri.js
@@ -1,32 +1,36 @@
-require('dotenv').config();
+// require("dotenv").config();
 
 var keys = require("./keys");
 
 var fs = require("fs");
 var twitter = require("twitter");
-var spotify = require("node-spotify-api");
+// var spotify = require("node-spotify-api");
 var request = require("request");
 
+var user = process.argv[2];
+
+var search = "";
+
+for (i = 3; i < process.argv.length; i++)
+    search += process.argv[i] + "+"
 
 
-var spotify = new spotify(keys.spotify);
+// var spotify = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
-function viewTweet (){
+function viewTweet() {
 
-    var params = {screen_name: "madisonnb7", count:3};
-    
-    client.get("statuses/user_timeline", params, function(error, tweets, response) {
-      if (error) {
-        console.log(error);
-      }
-      for(var i = 0; i < tweets.length; i++){
-        console.log("************");
-        console.log(tweets[i].text);
-        console.log("************");
-      }
-    
+    var params = { screen_name: "Madisonnb7", count: 5 };
+
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++)
+
+                console.log(tweets[i].text);
+
+        }
+
     });
-    }
-    
+}
+
 
