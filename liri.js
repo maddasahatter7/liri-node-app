a6082c2d8f19cb7ddb7ad506790518b5fb31f885
Matt Barnett <Madison@Madisons-MacBook-Pro.local>
2018-01-07 20:07:08 -0500

    cannot find modult dotenv issue working on resolution

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 
 var keys = require("./keys");
 
@@ -7,5 +7,26 @@ var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var request = require("request");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+
+var spotify = new spotify(keys.spotify);
+var client = new twitter(keys.twitter);
+
+function viewTweet (){
+
+    var params = {screen_name: "madisonnb7", count:3};
+    
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+      if (error) {
+        console.log(error);
+      }
+      for(var i = 0; i < tweets.length; i++){
+        console.log("************");
+        console.log(tweets[i].text);
+        console.log("************");
+      }
+    
+    });
+    }
+    
+
